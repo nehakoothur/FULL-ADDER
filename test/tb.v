397b44d0d35dc73d38c971c4ddcc5b337e4f8195
nehakoothur <nehakoothur@gmail.com>
2026-07-19 12:49:40 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,56 @@
-`default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb_full_adder;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    // Inputs
+    reg A;
+    reg B;
+    reg Cin;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    // Outputs
+    wire Sum;
+    wire Cout;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+    // Instantiate the Full Adder
+    full_adder uut (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+    // Apply test vectors
+    initial begin
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+        $display("A B Cin | Sum Cout");
+        $display("------------------");
+
+        A=0; B=0; Cin=0; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=0; B=0; Cin=1; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=0; B=1; Cin=0; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=0; B=1; Cin=1; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=1; B=0; Cin=0; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=1; B=0; Cin=1; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=1; B=1; Cin=0; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        A=1; B=1; Cin=1; #10;
+        $display("%b %b  %b  |  %b    %b", A,B,Cin,Sum,Cout);
+
+        $finish;
+    end
 
 endmodule
